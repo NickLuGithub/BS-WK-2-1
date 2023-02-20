c7f842c7b83c9e712636afe73f85ddd230763f69
NickLu <word4205@gmail.com>
2023-02-20 11:43:32 +0800

    完

--- ExLinqSample011/Program.cs
+++ ExLinqSample011/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,38 @@ namespace ExLinqSample011
     {
         static void Main(string[] args)
         {
+            var list = CreateList();
+            
+            //var order1 = list.OrderBy(x => x.Age);
+            //Display(order1);
+            //var order2 = list.OrderByDescending(x => x.Age);
+            //Display(order2);
+            //var order3 = list.OrderBy(x => x.Name).ThenBy(x => x.Age);
+            //Display(order3);
+            //var order4 = list.OrderByDescending(x => x.Name).ThenBy(x => x.Age);
+            //Display(order4);
+
+            var order1 = 
+                from o in list
+                orderby o.Name, o.Age
+                select o;
+            Display(order1);
+            var order2 =
+                from o in list
+                orderby o.Name descending, o.Age descending
+                select o;
+            Display(order2);
+
+            Console.ReadLine();
+        }
+
+        static void Display(IOrderedEnumerable<MyData> source)
+        { 
+            foreach(var item in source)
+            {
+                Console.WriteLine($"{item.Name}\t : {item.Age}");
+            }
+            Console.WriteLine("-------------------------"); 
         }
 
         static List<MyData> CreateList()
